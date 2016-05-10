2834d9e2d6b5bae89629f42064e9108c3a7da742
Trevor <thod0127@740s-pl-130.local>
2016-05-10 13:34:29 -0600

    Added player class with all required components

---
+++ DerpyGame/Model/Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace DerpyGame.Model
+{
+	public class Player
+	{
+		private int score;
+		private bool active;
+		private int health;
+
+		public Texture2D PlayerTexture;
+
+		public Vector2 Position;
+
+		public bool Active
+		{
+			get{ return active; }
+			set{ active = value; }
+		}
+			
+		public int Health
+		{
+			get{ return health; }
+			set{ health = value; }
+		}
+			
+		public int Width
+		{
+			get { return PlayerTexture.Width; }
+		}
+			
+		public int Height
+		{
+			get { return PlayerTexture.Height; }
+		}
+
+		public int Score
+		{
+			get{ return score; }
+			set{ score = value; }
+		}
+			
+		public void Initialize(Texture2D texture, Vector2 position)
+		{
+			PlayerTexture = texture; 
+			Position = position;
+			this.active = true;
+			this.health = 100;
+			this.score = 0;
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{ 
+			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+		}
+
+		public void Update()
+		{
+
+		}
+	}
+}
+
