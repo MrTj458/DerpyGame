d3b01b064ab57f301d71c710cc261e6de152f95d
Trevor <thod0127@740s-pl-130.local>
2016-05-10 13:14:31 -0600

    Added media content to project pipeline

--- DerpyGame/Controller/SpaceGame.cs
+++ DerpyGame/Controller/SpaceGame.cs
@@ -71,7 +71,7 @@ namespace DerpyGame.Controller
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw (GameTime gameTime)
 		{
-			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+			graphics.GraphicsDevice.Clear (Color.Green);
             
 			//TODO: Add your drawing code here
             
